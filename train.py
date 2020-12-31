fix off by one for distance bins

train.py
@@ -4,9 +4,9 @@ from torch.utils.data import DataLoader
 import torch.nn.functional as F
 from einops import rearrange
 
+import sidechainnet as scn
 from alphafold2_pytorch import Alphafold2
 
-import sidechainnet as scn
 
 # constants
 
@@ -15,6 +15,7 @@ GRADIENT_ACCUMULATE_EVERY = 16
 LEARNING_RATE = 3e-4
 IGNORE_INDEX = -100
 THRESHOLD_LENGTH = 250
+DISTANCE_BINS = 37
 
 # helpers
 
@@ -29,11 +30,14 @@ def derive_mask(coords):
     coords_center = coords[:, :, :2].mean(dim = 2) # mean of coordinates of N, Cα, C
     return coords_center.sum(dim = -1) == 0
 
-def get_bucketed_distance_matrix(coords):
+def get_bucketed_distance_matrix(coords, mask):
     coords_center = coords[:, :, :2].mean(dim = 2) # mean of coordinates of N, Cα, C
     distances = ((coords_center[:, :, None, :] - coords_center[:, None, :, :]) ** 2).sum(dim = -1).sqrt()
-    boundaries = torch.linspace(2, 20, steps = 37, device = coords.device)
-    return torch.bucketize(distances, boundaries)
+    boundaries = torch.linspace(2, 20, steps = DISTANCE_BINS, device = coords.device)
+    discretized_distances = torch.bucketize(distances, boundaries)
+    discretized_distances.masked_fill_(discretized_distances == DISTANCE_BINS, (DISTANCE_BINS - 1))
+    discretized_distances.masked_fill_(mask[:, :, None] | mask[:, None, :], IGNORE_INDEX)
+    return discretized_distances
 
 # get data
 
@@ -75,8 +79,7 @@ for _ in range(NUM_BATCHES):
         coords = rearrange(coords, 'b (l c) d -> b l c d', l = l)
         mask = derive_mask(coords)
 
-        discretized_distances = get_bucketed_distance_matrix(coords)
-        discretized_distances.masked_fill_(mask[:, :, None] | mask[:, None, :], IGNORE_INDEX)
+        discretized_distances = get_bucketed_distance_matrix(coords, mask)
 
         # predict
 
@@ -98,5 +101,6 @@ for _ in range(NUM_BATCHES):
         loss.backward()
 
     print('loss:', loss.item())
+
     optim.step()
     optim.zero_grad()