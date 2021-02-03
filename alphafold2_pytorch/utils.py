7945bbbb62c8e83dc12df76a64353bd1fe5545e8
hypnopump <ericalcaide1@gmail.com>
2021-02-03 19:01:13 +0100

    shape corrected

--- alphafold2_pytorch/utils.py
+++ alphafold2_pytorch/utils.py
@@ -237,7 +237,7 @@ def center_distogram_torch(distogram, bins=DISTANCE_THRESHOLDS, min_t=1., center
         cum_dist = torch.cumsum(distogram, dim=-1)
         medium   = 0.5 * torch.ones(*cum_dist.shape[:-1], device=device).unsqueeze(dim=-1)
         central  = torch.searchsorted(cum_dist, medium).squeeze()
-        central  = n_bins[ torch.minimum(central, torch.tensor( len(n_bins)-1 )).long() ]
+        central  = n_bins[ torch.minimum(central, torch.tensor( len(n_bins)-1 )).long() ].unsqueeze(dim=0)
     elif center == "mean":
         central  = (distogram * n_bins).sum(dim=-1)
     # create mask for last class - (IGNORE_INDEX)   
