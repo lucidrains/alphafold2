dca4be5f987208de70b3a9c40eea1e4c5dcd6612
Phil Wang <lucidrains@gmail.com>
2021-02-04 09:02:39 -0800

    add a test for the main Alphafold2 class

---
+++ tests/test_attention.py
@@ -0,0 +1,22 @@
+import torch
+from alphafold2_pytorch.alphafold2 import Alphafold2
+
+def test_main():
+    model = Alphafold2(
+        dim = 256,
+        depth = 2,
+        heads = 8,
+        dim_head = 64
+    )
+
+    seq = torch.randint(0, 21, (2, 128))
+    msa = torch.randint(0, 21, (2, 5, 64))
+    mask = torch.ones_like(seq).bool()
+    msa_mask = torch.ones_like(msa).bool()
+
+    distogram = model(
+        seq,
+        msa,
+        mask = mask,
+        msa_mask = msa_mask
+    )
