8b9633c280e569a9c8c77b254e4fa70896f49f8c
Ersi Ni <ersi.ni@nilbot.net>
2021-02-11 17:42:44 +0000

    export more functions

--- alphafold2_pytorch/__init__.py
+++ alphafold2_pytorch/__init__.py
@@ -1,1 +1,1 @@
-from alphafold2_pytorch.alphafold2 import Alphafold2, Attention, SparseAttention, AxialAttention
+from alphafold2_pytorch.alphafold2 import Alphafold2, Attention, SparseAttention, AxialAttention, exists, default, PreNorm, PreNormCross, GEGLU, FeedForward, SequentialSequence
