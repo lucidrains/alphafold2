0c323254aaaaffa0a3175da906300f91964f9580
Phil Wang <lucidrains@gmail.com>
2021-04-07 09:15:30 -0700

    enforce version of se3 transformer

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'alphafold2-pytorch',
   packages = find_packages(),
-  version = '0.0.74',
+  version = '0.0.75',
   license='MIT',
   description = 'AlphaFold2 - Pytorch',
   author = 'Phil Wang, Eric Alcaide',
@@ -23,7 +23,7 @@ setup(
     'proDy',
     'pytorch-lightning',
     'requests',
-    'se3-transformer-pytorch>=0.2.1',
+    'se3-transformer-pytorch>=0.2.5',
     'sidechainnet',
     'torch>=1.6',
     'tqdm',
