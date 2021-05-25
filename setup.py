af1351271195a4feacdfa52b23eb6599e68ac6d4
Phil Wang <lucidrains@gmail.com>
2021-05-25 11:05:22 -0700

    0.1.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'alphafold2-pytorch',
   packages = find_packages(),
-  version = '0.1.4',
+  version = '0.1.5',
   license='MIT',
   description = 'AlphaFold2 - Pytorch',
   author = 'Phil Wang, Eric Alcaide',
