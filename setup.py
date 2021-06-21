020008cbebd46ab8947fa9072570b289b2248ae1
Phil Wang <lucidrains@gmail.com>
2021-06-21 11:11:51 -0700

    deprecate MDS in favor of using Graph Transformer for constituting trunk to initial set of coordinates for refinement, given new Allan Costa and Baker lab papers

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'alphafold2-pytorch',
   packages = find_packages(),
-  version = '0.2.2',
+  version = '0.3.0',
   license='MIT',
   description = 'AlphaFold2 - Pytorch',
   author = 'Phil Wang, Eric Alcaide',
